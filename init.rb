59a8ab9a4b54b0a6d67c4f7ae1c828f4e63c295b
Jun Okumura <jun.okumura@gmail.com>
2011-09-07 00:15:32 +0900

    create plugin skelton

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_svn_account_manager do
+  name 'Redmine Svn Account Manager plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
