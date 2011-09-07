06f0edf80655abe4ab1b6fd8c63cdf6827b895c6
Jun Okumura <jun.okumura@gmail.com>
2011-09-08 03:18:32 +0900

    plugin urlと作者urlをgithubに指定

--- init.rb
+++ init.rb
@@ -2,9 +2,9 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_svn_account_manager do
   name 'Redmine Svn Account Manager plugin'
-  author 'Author name'
+  author 'Jun Okumura'
   description 'This is a plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/oku23/redmine_svn_account_manager'
+  author_url 'https://github.com/oku23'
 end
