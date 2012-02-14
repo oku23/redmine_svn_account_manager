aa096df78e74b57baa945949112ec216b0a53b4d
Jun Okumura <jun.okumura@gmail.com>
2012-02-14 19:49:28 +0900

    コミットミスを再コミット

--- app/controllers/svn_authzs_controller.rb
+++ app/controllers/svn_authzs_controller.rb
@@ -5,18 +5,6 @@ class SvnAuthzsController < ApplicationController
   def index
     @directories = init()
   end
-
-  def new
-    @directory = SvnDirectory.new(params[:svn_directory])
-    @directory.project_id = @project.id
-    if @directory.save
-      flash[:notice] = l(:succcessful_create_svn_directory)
-      redirect_to :action => 'index', :id => @project
-    else
-      @directories = init()
-      render :action => 'index'
-    end
-  end
   
   def init
     SvnDirectory.find(:all, :conditions => ["project_id = #{@project.id}"])
---
+++ app/controllers/svn_directories_controller.rb
@@ -0,0 +1,20 @@
+class SvnDirectoriesController < ApplicationController
+  unloadable
+  before_filter :find_project, :authorize
+
+  def new
+    @directory = SvnDirectory.new(params[:svn_directory])
+    @directory.project_id = @project.id
+    if @directory.save
+      flash[:notice] = l(:succcessful_create_svn_directory)
+      redirect_to :controller => 'svn_authzs', :action => 'index', :id => @project
+    else
+      @directories = init()
+      render :controller => 'svn_authzs', :action => 'index'
+    end
+  end
+  
+  def init
+    SvnDirectory.find(:all, :conditions => ["project_id = #{@project.id}"])
+  end
+end
